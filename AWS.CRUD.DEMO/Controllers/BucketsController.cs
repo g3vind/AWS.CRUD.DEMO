0b748580138f8598d0cc82443d38807b04184210
Govind Kumar <g3vind@gmail.com>
2024-07-21 18:30:18 +0530

    Add project files.

---
+++ AWS.CRUD.DEMO/Controllers/BucketsController.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.S3;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AWS.CRUD.DEMO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BucketsController : ControllerBase
+    {
+        private readonly IAmazonS3 _amazons3;
+        public BucketsController(IAmazonS3 amazons3)
+        {
+            _amazons3 = amazons3;
+        }
+        [HttpGet("/list")]
+        public async Task<IActionResult> ListAsync()
+        {
+            var data = await _amazons3.ListBucketsAsync();
+            var buckets = data.Buckets.Select(b => { return b.BucketName; });
+            return Ok(buckets);
+        }
+    }
+}
